6458e7806a22a7ddabfec7a0af81ba014a1af19d
fromddy <ddy_mainland@163.com>
2024-10-17 22:10:12 +0800

    fix: planet owner in PlanetContextPaneContent

--- packages/client/src/Frontend/Panes/PlanetContextPane.tsx
+++ packages/client/src/Frontend/Panes/PlanetContextPane.tsx
@@ -66,15 +66,13 @@ function PlanetContextPaneContent({
   onToggleSendForces: () => void;
   onToggleAbandon: () => void;
 }) {
-  const mainAccount = useAccount(uiManager);
   const account = useAccount(uiManager);
   const notifs = useMemo(
     () => getNotifsForPlanet(planet.value, account),
     [planet, account],
   );
 
-  const owned =
-    planet.value?.owner === account || planet.value?.owner === mainAccount;
+  const owned = planet.value?.owner === account;
 
   useEffect(() => {
     if (!planet.value) {
