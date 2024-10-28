977d313947b926e973a117f598205508f2c9847b
fromddy <ddy_mainland@163.com>
2024-10-28 22:47:45 +0800

    chore: change LOW_BALANCE_THRESHOLD

--- packages/client/src/wallet/utils.ts
+++ packages/client/src/wallet/utils.ts
@@ -1,7 +1,7 @@
 import { type Hex, numberToHex, padHex, parseEther, stringToHex } from "viem";
 
 export const MINIMUM_BALANCE = parseEther("0.000006");
-export const LOW_BALANCE_THRESHOLD = parseEther("0.0005");
+export const LOW_BALANCE_THRESHOLD = parseEther("0.00001");
 export const RECOMMENDED_BALANCE = parseEther("0.005");
 export const zeroAddress = padHex(numberToHex(0, { size: 4 }), {
   size: 20,
